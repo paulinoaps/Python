3d24317376b827cef1ccdcba8a9eb25e2c7a1390
Alexandre Paulino da Silva <silva.alexandrepaulino@gmail.com>
2025-06-17 11:38:40 -0300

    Update 003.py

---
+++ Learning and Lessons/140 + Basic Python Programs/003.py
@@ -0,0 +1,7 @@
+# Insira a base e a altura
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+# álculo da área do triângulo
+area = 0.5 * base * altura
+# Impressão do resultado
+print(f"A área do triângulo é: {area}")
